1114c68a7c472d79d9365a7845e526062dce8782
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-06-12 22:24:21 +0530

    Day 12 files are added

---
+++ JavaScript/Day 12/operators.js
@@ -0,0 +1,72 @@
+// console.log(3 ** 3);
+
+// var x = 1;
+// x %= 2;
+// // x = x * 2
+// console.log(x);
+// console.log(1 - "1") // 11 // 0
+// console.log(1 == "1");// true
+
+// 1 == 1
+// console.log(1)
+// console.log(1 === "1")
+
+// number == string
+// var x;
+// var x = null;
+// console.log(undefined === null)
+
+// console.log(2 > 3)
+
+
+// var isWarm =  true;
+// var isRainy = false;
+// console.log(isWarm || isRainy);
+
+// true && false = false
+
+
+
+
+
+
+
+// console.log( 5 | 3);
+// binary value 5 = 1 0 1
+// Binary value 3 = 0 1 1
+
+                // 1   = 0 0 1
+                        //   1  1 1
+
+var x = 1;
+// x--;
+// ++x; pre fix
+// x++;post fix
+
+// var y = ++x;
+// console.log(y++)
+// console.log(x)
+// var x = null;
+// name:mahesh
+
+// console.log("code1");
+// console.log("code2");
+// console.log("code3");
+// console.log("code4");
+
+
+var trafficLight = "Green";
+// var trafficLight = "Yellow";
+// console.log(trafficLight)
+// console.log(trafficLight == "Green");
+
+
+if(trafficLight ==  "Green"){
+    console.log("Green Light: Please move");
+}
+else if(trafficLight == "Yellow"){
+    console.log("Yellow Light: Get ready to move");
+}
+else{
+    console.log("Please stop");
+}
