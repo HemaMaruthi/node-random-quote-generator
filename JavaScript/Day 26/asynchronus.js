d0cf782933d8967809eed983750c18c4f9e53319
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-06-29 22:13:09 +0530

    Day 27 code is added

--- JavaScript/Day 26/asynchronus.js
+++ JavaScript/Day 26/asynchronus.js
@@ -1,50 +1,50 @@
 // console.log("This is one")
 // console.log("This is three")
 
-let batch  = (a,b)=>{
-    console.log(`${a} ${b}`)
-}
-setTimeout(()=>{
-    console.log("This is Two")
+// let batch  = (a,b)=>{
+//     console.log(`${a} ${b}`)
+// }
+// setTimeout(()=>{
+//     console.log("This is Two")
 
-},4000,"Hema","Coding")
+// },4000,"Hema","Coding")
 
-let stopInterval = setInterval(()=>{
-    console.log("This is set Interval")
+// let stopInterval = setInterval(()=>{
+//     console.log("This is set Interval")
 
-},1000,"Hema","Coding")
+// },1000,"Hema","Coding")
 
-setTimeout(()=>{
-    console.log("stopped Interval")
+// setTimeout(()=>{
+//     console.log("stopped Interval")
 
-    clearInterval(stopInterval)
-},3000)
+//     clearInterval(stopInterval)
+// },3000)
 
-let callBackHell = (callback) => {
-  setTimeout(() => {
-    callback();
-  }, 1000);
-};
-callBackHell(() => {
-  console.log("Function 1");
-  callBackHell(() => {
-    console.log("Function 2");
-    callBackHell(() => {
-      console.log("Function 3 ");
-    });
-  });
-});
+// let callBackHell = (callback) => {
+//   setTimeout(() => {
+//     callback();
+//   }, 1000);
+// };
+// callBackHell(() => {
+//   console.log("Function 1");
+//   callBackHell(() => {
+//     console.log("Function 2");
+//     callBackHell(() => {
+//       console.log("Function 3 ");
+//     });
+//   });
+// });
 
-let getPromise = new Promise((resolve,reject)=>{
-    console.log("This is Promise")
-    resolve("Success")
-    // reject("This is rejected")
-})
+// let getPromise = new Promise((resolve,reject)=>{
+//     console.log("This is Promise")
+//     resolve("Success")
+//     // reject("This is rejected")
+// })
 
 let newPromise = (number) => {
   return new Promise((resolve, reject) => {
-    console.log(`This promise ${number}`)
     setTimeout(() => {
+      console.log(`This promise ${number}`)
         resolve(`This is resolve ${number}`);
     }, 1000);
     // reject(`This is reject ${number}`)
@@ -59,15 +59,15 @@ let newPromise = (number) => {
 //     console.log(rej)
 // })
 
-newPromise(1).then((res) => {
-  console.log(res);
-  newPromise(2).then((res) => {
-    console.log(res);
-    newPromise(3).then((res) => {
-      console.log(res);
-    });
-  });
-});
+// newPromise(1).then((res) => {
+//   console.log(res);
+//   newPromise(2).then((res) => {
+//     console.log(res);
+//     newPromise(3).then((res) => {
+//       console.log(res);
+//     });
+//   });
+// });
 
 let asyncFunction = async function () {
   console.log("This is Async function 1");
