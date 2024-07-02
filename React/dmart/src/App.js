3e35a00756874a77aea13fdc1cf179004c27e1e6
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-07-02 22:38:57 +0530

    Day 02 JSX and Component

---
+++ React/dmart/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CreateImage from './CreateImage.js'
+
+function App() {
+  return (
+    <div className="App">
+      <div></div>
+      <CreateImage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/dmart/src/CreateImage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CreateImage extends Component {
+  render() {
+    return (
+      <div>CreateImage</div>
+    )
+  }
+}
