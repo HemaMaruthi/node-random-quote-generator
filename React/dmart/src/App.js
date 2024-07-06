9b4fac308b1f4b55fd4d0961c77605b61f84c104
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-07-06 22:12:18 +0530

    Day 06 API calls

--- React/dmart/src/App.js
+++ React/dmart/src/App.js
@@ -1,15 +1,41 @@
-import './App.css';
+import "./App.css";
 // import FunctionBased from './Component/FunctionBased.js';
 // import ClassBased from './Component/ClassBased.js';
 // import StateManage from './Component/Hook/StateManage.js';
 
 // import Groceries from './Component/InventoryDashboard/Groceries.js';
 // import PackagedGoods from './Component/InventoryDashboard/PackagedGoods.js';
-// import ListItems from './Component/InventoryDashboard/ListItems.js';
-import task from './Component/InventoryDashboard/ListItems.js';
+// import ListItems from "./Component/InventoryDashboard/ListItems.js";
+import Electronics from "./Component/InventoryDashboard/Electronics.js";
 
 
 function App() {
+  return (
+    <div>
+    <div className="nav-bar">
+      <div className="logo">
+        <img src="https://logowik.com/content/uploads/images/dmart-avenue-supermarts4302.jpg" />
+      </div>
+      <div className="nav-link" href="#">
+        Home
+      </div>
+      <div className="nav-link" href="#">
+        Electronics
+      </div>
+      <div className="nav-link" href="#">
+        Home
+      </div>
+      <div className="nav-link" href="#">
+        Home
+      </div>
+      <div className="nav-link" href="#">
+        Home
+      </div>
+    </div>
+
+    <Electronics/>
+    </div>
+  );
   // let showComponent = false;
 
   // if(showComponent){
@@ -17,14 +43,13 @@ function App() {
   // }else{
   //  return <ClassBased/>
   // }
-//  return showComponent ? <FunctionBased/> : <ClassBased/>
+  //  return showComponent ? <FunctionBased/> : <ClassBased/>
 
-// return <StateManage/>
-// return <Groceries/>
-// return <PackagedGoods/>
+  // return <StateManage/>
+  // return <Groceries/>
+  // return <PackagedGoods/>
 
-return <ListItems/>
-
+  // return <ListItems/>
 
   // return (
   //   <div className="App">
---
+++ React/dmart/src/Component/InventoryDashboard/Electronics.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Electronics.css";
+function Electronics() {
+  let [electronics, setElectronics] = useState([
+    {
+      title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+      description:
+        "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+      image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+      rating: {
+        rate: 3.3,
+        count: 203,
+      },
+    },
+  ]);
+  let getElectronics = () => {
+    fetch("https://fakestoreapi.com/products/category/electronics")
+      .then((res) => {
+        // console.log(res.json())
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        return setElectronics(data);
+      });
+  };
+  return (
+    <div className="container">
+    <div className="electronics-container">
+      {electronics.map((device) => {
+        console.log(device);
+
+        return (
+          <div className="device-container">
+            <img src={device.image} width="200px" height="200px" />
+            <div className="device-details">
+              <h2>{device.title}</h2>
+            </div>
+            <div>{device.description}</div>
+            <div>Rating: {device.rating.rate}</div>
+            <div>{device.rating.count} review</div>
+          </div>
+        );
+      })}
+    </div>
+      <button onClick={getElectronics}>Electronics</button>
+
+    </div>
+  );
+}
+
+export default Electronics;
